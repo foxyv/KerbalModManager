a12f53a6ac247cceda541672c623714460e49aaa
Verna Smith <Verna Smith@PinkiePC>
2014-03-30 13:18:19 -0700

    More initial commit changes.
    Signed-off-by: Verna Smith <Verna Smith@PinkiePC>

--- src/main/java/us/vernabean/kerbal/mod/manager/ui/KerbalModManagerUI.java
+++ src/main/java/us/vernabean/kerbal/mod/manager/ui/KerbalModManagerUI.java
@@ -7,7 +7,8 @@ public class KerbalModManagerUI extends JFrame {
 	private static final long serialVersionUID = -8104891259168367222L;
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		KerbalModManagerUI ui = new KerbalModManagerUI();
+		ui.setVisible(true);
 
 	}
 
